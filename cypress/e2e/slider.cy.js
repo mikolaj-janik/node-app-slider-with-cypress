67d5e297c188e3eea72a7ee2c72f232f247024bb
Mikołaj Janik <mikolaj.j003@interia.pl>
2025-03-13 21:15:45 +0100

    cypress and hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,33 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Verify if each slide is being displayed correctly', function () {
+    cy.visit('http://localhost:3000');
+
+    // first slide
+    cy.get('.card-description').should('contain.text', 'Rome');
+    cy.get('.card-description').should('contain.text', 'Italy');
+    cy.get('.swiper-button-next').should('be.visible');
+    cy.get('.swiper-button-next').should('not.be.disabled');
+    cy.get('.swiper-button-next').click();
+    cy.wait(500);
+
+    // second slide
+    cy.get('.card-description').should('contain.text', 'London');
+    cy.get('.card-description').should('contain.text', 'United Kingdom');
+    cy.get('.swiper-button-next').should('be.visible');
+    cy.get('.swiper-button-next').should('not.be.disabled');
+    cy.get('.swiper-button-next').click();
+    cy.wait(500);
+
+    // third slide
+    cy.get('.card-description').should('contain.text', 'Paris');
+    cy.get('.card-description').should('contain.text', 'France');
+    cy.get('.swiper-button-next').should('be.visible');
+    cy.get('.swiper-button-next').should('not.be.disabled');
+    cy.get('.swiper-button-next').click();
+    cy.wait(500);
+  });
+});
